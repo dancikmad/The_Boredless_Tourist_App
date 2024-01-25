4af8fc9e50bfb7cf1391cb0381e3d3e2b96e0062
Daniel Crigan <crgdaniel@yahoo.com>
2024-01-25 13:24:32 +0000

    Added test objects

---
+++ script.py
@@ -0,0 +1,12 @@
+destinations = [
+    "Paris, France",
+    "Shanghai China",
+    "Los Angeles, Usa",
+    "Sao Paulo, Brazil",
+    "Cairo, Egypt",
+]
+
+# Define a test traveler to check the functionality
+# test_traveler is the user of the app
+test_traveler = ["Erin Wilkes", "Shanghai China", ["historical site", "art"]]
+
