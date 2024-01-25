7a8c0f2e090ae2fa296211a93a98cbbe12b0788e
Daniel Crigan <crgdaniel@yahoo.com>
2024-01-25 14:05:50 +0000

    Added interest finder logic

--- script.py
+++ script.py
@@ -1,14 +1,14 @@
 destinations = [
     "Paris, France",
-    "Shanghai China",
-    "Los Angeles, Usa",
-    "Sao Paulo, Brazil",
+    "Shanghai, China",
+    "Los Angeles, USA",
+    "São Paulo, Brazil",
     "Cairo, Egypt",
 ]
 
 # Define a test traveler to check the functionality
 # test_traveler is the user of the app
-test_traveler = ["Erin Wilkes", "Shanghai China", ["historical site", "art"]]
+test_traveler = ["Erin Wilkes", "Shanghai, China", ["historical site", "art"]]
 
 
 def get_destination_index(destination):
@@ -52,4 +52,21 @@ add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
 add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
 add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
 add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+
+# Feature: Finding the Best Places to Go
+def find_attractions(destination, interests):
+    destination_index = get_destination_index(destination)
+    attractions_in_city = attractions[destination_index]
+    attractions_with_interest = []
+    
+    # Ommiting tags showing to the user
+    for attraction in attractions_in_city:
+        possible_attraction = attraction 
+        attraction_tags = attraction[1]
+        
+        for interest in interests:
+            if interest in attraction_tags:
+                attractions_with_interest.append(possible_attraction[0])
+    
+    return attractions_with_interest
 
