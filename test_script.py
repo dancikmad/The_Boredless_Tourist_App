78ede0489c4da72ed78535e5036036e2e83804e7
Daniel Crigan <crgdaniel@yahoo.com>
2024-01-25 14:34:34 +0000

    Refactored tests

--- test_script.py
+++ test_script.py
@@ -1,18 +1,11 @@
-import pytest
-from script import add_attraction, find_attractions, get_attractions_for_traveler, attractionssc
 
-# Additional test cases for the script module
-def test_add_attraction():
-    add_attraction("Paris, France", ["Eiffel Tower", ["landmark", "viewing deck"]])
-    assert attractions[0] == [["the Louvre", ["art", "museum"]],
-                               ["Arc de Triomphe", ["historical site", "monument"]],
-                               ["Eiffel Tower", ["landmark", "viewing deck"]]]
+import pytest
+from script import get_destination_index, get_traveler_location, test_traveler, destinations
+
+# Testing the functions in script module
+def test_get_destination_index():
+    assert get_destination_index("Los Angeles, USA") == 2
+    assert get_destination_index("Paris, France") == 0
     
-def test_find_attractions():
-    assert find_attractions("Shanghai, China", ["garden"]) == ["Yu Garden"]
-    assert find_attractions("Los Angeles, USA", ["art"]) == ["LACMA"]
-    assert find_attractions("Cairo, Egypt", ["historical site", "museum"]) == ["Pyramids of Giza", "Egyptian Museum"]
-    
-def test_get_attractions_for_traveler():
-    traveler = ["John Doe", "São Paulo, Brazil", ["zoo", "historical site"]]
-    assert get_attractions_for_traveler(traveler) == "Hi John Doe, we think you'll like these places around São Paulo, Brazil: the São Paulo Zoo, the Pátio do Colégio."
+def test_get_traveler_location():
+    assert get_traveler_location(test_traveler) == 1
