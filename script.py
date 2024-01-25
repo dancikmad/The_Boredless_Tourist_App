d2e62b0ee6318445ea55868ee2cf13c6e62aceeb
Daniel Crigan <crgdaniel@yahoo.com>
2024-01-25 13:37:43 +0000

    Added logic to find traveler destinations and convert to internal data

--- script.py
+++ script.py
@@ -10,3 +10,17 @@ destinations = [
 # test_traveler is the user of the app
 test_traveler = ["Erin Wilkes", "Shanghai China", ["historical site", "art"]]
 
+
+def get_destination_index(destination):
+    """A function that identifies each location based on index and 
+    returns it's index from the list"""
+    destination_index = destinations.index(destination)
+
+    return destination_index
+
+def get_traveler_location(traveler):
+    traveler_destination = traveler[1]
+    traveler_destination_index = get_destination_index(traveler_destination)
+    
+    return traveler_destination_index
+
