91d8ea694ea7db16a407997fde8c3f4232b3ff41
Daniel Crigan <crgdaniel@yahoo.com>
2024-01-25 14:22:11 +0000

    Refactored tests

--- test_script.py
+++ test_script.py
@@ -1,10 +1,18 @@
 import pytest
-from script import get_destination_index, get_traveler_location, test_traveler
+from script import add_attraction, find_attractions, get_attractions_for_traveler, attractionssc
 
-# Testing the functions in script module
-def test_get_destination_index():
-    assert get_destination_index("Los Angeles, Usa") == 2
-    assert get_destination_index("Paris, France") == 0
+# Additional test cases for the script module
+def test_add_attraction():
+    add_attraction("Paris, France", ["Eiffel Tower", ["landmark", "viewing deck"]])
+    assert attractions[0] == [["the Louvre", ["art", "museum"]],
+                               ["Arc de Triomphe", ["historical site", "monument"]],
+                               ["Eiffel Tower", ["landmark", "viewing deck"]]]
     
-def test_get_traveler_location():
-    assert get_traveler_location(test_traveler) == 1
+def test_find_attractions():
+    assert find_attractions("Shanghai, China", ["garden"]) == ["Yu Garden"]
+    assert find_attractions("Los Angeles, USA", ["art"]) == ["LACMA"]
+    assert find_attractions("Cairo, Egypt", ["historical site", "museum"]) == ["Pyramids of Giza", "Egyptian Museum"]
+    
+def test_get_attractions_for_traveler():
+    traveler = ["John Doe", "São Paulo, Brazil", ["zoo", "historical site"]]
+    assert get_attractions_for_traveler(traveler) == "Hi John Doe, we think you'll like these places around São Paulo, Brazil: the São Paulo Zoo, the Pátio do Colégio."
