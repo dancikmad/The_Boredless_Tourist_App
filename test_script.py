de0a42bc2ac5be9bd7c657e06bc06f7d9656f88a
Daniel Crigan <crgdaniel@yahoo.com>
2024-01-25 13:38:06 +0000

    Tested get_traveler_location function

--- test_script.py
+++ test_script.py
@@ -1,7 +1,10 @@
 import pytest
-from script import get_destination_index
+from script import get_destination_index, get_traveler_location, test_traveler
 
 # Testing the functions in script module
 def test_get_destination_index():
     assert get_destination_index("Los Angeles, Usa") == 2
+    assert get_destination_index("Paris, France") == 0
     
+def test_get_traveler_location():
+    assert get_traveler_location(test_traveler) == 1
